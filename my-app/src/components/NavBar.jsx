06bd793b477ba290c3fcd9adc6a1fd14ee51de61
micvant <micvant@mail.ru>
2020-02-20 22:21:04 +0300

    add css for components

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import classes from '../css/NavBar.module.css';
+// .module.css превращает css файл в объект
+// {classes.nav} позволяет получить данные из класа css
 
 const NavBar = () => {
     return (
-        <nav className="nav">
-        <div>
-          <a href="#s">Profile</a>
-        </div>
-        <div>
-          <a href="#s">Messages</a>
-        </div>
-        <div>
-          <a href="#s">News</a>
-        </div>
-        <div>
-          <a href="#s">Music</a>
-        </div>
-        <div>
-          <a href="#s">Setings</a>
-        </div>
-      </nav>
+        <nav className={classes.nav}>
+          <div className={classes.item}>
+            <a>Profile</a>
+          </div>
+          <div className={classes.item}>
+            <a>Messages</a>
+          </div>
+          <div className={classes.item}>
+            <a>News</a>
+          </div>
+          <div className={classes.item}>
+            <a>Music</a>
+          </div>
+          <div className={classes.item}>
+            <a>Setings</a>
+          </div>
+        </nav>
     );
 };
 
--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import '../css/Profile.css';
 
 const Profile = () => {
     return (
         <div className="content">
-        <div>
-          <img src="https://cdn.pixabay.com/photo/2016/04/15/04/02/water-1330252__340.jpg" alt=""></img>
-        </div>
-        <div>
-          ava + descriptions
-        </div>
-        <div>
-          My posts
-          <div>New post</div>
-        </div>
-        <div>
-            <div>Post 1</div>
-            <div>Post 1</div>
-        </div>
-      </div> 
+          <div>
+            <img src="https://cdn.pixabay.com/photo/2016/04/15/04/02/water-1330252__340.jpg" alt=""></img>
+          </div>
+          <div>
+            ava + descriptions
+          </div>
+          <div>
+            My posts
+            <div>New post</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 1</div>
+            </div>
+          </div>
+        </div> 
     );
 };
 
